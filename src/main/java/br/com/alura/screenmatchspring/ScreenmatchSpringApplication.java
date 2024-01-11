987b85727040f904ed74d065eaf1b77f0e4dd0f3
lgfas <luis.guilherme2811@gmail.com>
2024-01-11 11:57:10 -0300

    curso01.01.07

--- src/main/java/br/com/alura/screenmatchspring/ScreenmatchSpringApplication.java
+++ src/main/java/br/com/alura/screenmatchspring/ScreenmatchSpringApplication.java
@@ -1,5 +1,8 @@
 package br.com.alura.screenmatchspring;
 
+import br.com.alura.screenmatchspring.model.DadosSerie;
+import br.com.alura.screenmatchspring.service.ConsumoApi;
+import br.com.alura.screenmatchspring.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +16,13 @@ public class ScreenmatchSpringApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Hello, World!");
+        var consumoApi = new ConsumoApi();
+        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=eab1e0f8");
+        //System.out.println(json);
+        //json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+        System.out.println(json);
+        ConverteDados conversor = new ConverteDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
     }
 }
