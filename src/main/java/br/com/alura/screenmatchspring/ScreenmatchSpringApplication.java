66b9215a1d7ad6f328e5deeb4697060b43b3d7b3
lgfas <luis.guilherme2811@gmail.com>
2024-01-18 11:56:03 -0300

    api + front - 05.09

--- src/main/java/br/com/alura/screenmatchspring/ScreenmatchSpringApplication.java
+++ src/main/java/br/com/alura/screenmatchspring/ScreenmatchSpringApplication.java
@@ -3,26 +3,14 @@ package br.com.alura.screenmatchspring;
 import br.com.alura.screenmatchspring.main.Main;
 import br.com.alura.screenmatchspring.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchSpringApplication implements CommandLineRunner {
-
-    @Autowired
-    private SerieRepository serieRepository;
+public class ScreenmatchSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScreenmatchSpringApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-
-        Main main = new Main(serieRepository);
-
-        main.exibeMenu();
-
-    }
 }
